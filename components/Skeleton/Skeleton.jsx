b408b48d648d633431c153f98443448d32693a2a
PatekBosco <workspacese@gmail.com>
2024-08-29 20:32:38 +0300

    add skeleton for news and banner

--- components/Skeleton/Skeleton.jsx
+++ components/Skeleton/Skeleton.jsx
@@ -4,15 +4,15 @@ const Skeleton = ({count=1, type='banner'}) => {
     return (
        <>
        {count > 1 ? (
-        <ul>
-            {[Array(count)].map((_, index) => (
-                <li key={index} className={typr === "banner" ? styles.banner : styles.item}>
+        <ul className={styles.list}>
+            {[...Array(count)].map((_, index) => (
+                <li key={index} className={type === "banner" ? styles.banner : styles.item}>
 
                 </li>
             ))}
         </ul>
        ) : ( 
-       <li className={typr === "banner" ? styles.banner : styles.item}></li>
+       <li className={type === "banner" ? styles.banner : styles.item}></li>
        )}
        </>
     );
--- components/pages/Main/Main.jsx
+++ components/pages/Main/Main.jsx
@@ -7,24 +7,27 @@ import NewsList from '../../NewsList/NewsList';
 import Skeleton from '../../Skeleton/Skeleton';
 
 const Main = () => {
-    const [news, setNews] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+    const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     useEffect(() => {
         const fetchNews = async () => {
             try {
-                const response = await getNews()
-                setNews(response.news)
+                setIsLoading(true);
+                const response = await getNews();
+                setNews(response.news);
+                setIsLoading(false);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
         }
         fetchNews();
-    }, [])
+    }, []);
+
     return (
         <main className={styles.main}>
-            {news.length > 0 && !isLoading ? <NewsBanner item={news[0]} /> : <Skeleton type={'banner'} count={1}/>}
+            {news.length > 0 && !isLoading ? (<NewsBanner item={news[0]} />) : (<Skeleton type={"banner"} count={1} />)}
 
-            <NewsList news={news} />
+            {!isLoading ? (<NewsList news={news} />) : (<Skeleton type={"item"} count={10} />)}
         </main>
     );
 };
